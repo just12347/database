cfa9f44dbd14e273ab6ebcdc9a77899db6a6a607
mikeyandleo <b01705031@ntu.edu.tw>
2015-01-20 04:42:30 +0800

    php

---
+++ index.php
@@ -0,0 +1,152 @@
+<!-- 設定網頁編碼為UTF-8 -->
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	</head>	
+	
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php">Index</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li >
+							<a href="sec.php">Sec</a>
+						</li>
+						<li>
+							<a href="Ranking.php">Ranking</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ranking<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="Ranking-chi.php">國語</a>
+								</li>
+								<li>
+									<a href="Ranking-tai.php">台語</a>
+								</li>
+								<li>
+									<a href="Ranking-kor.php">韓語</a>
+								</li>
+								<li>
+									<a href="Ranking.php">English</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<form class="navbar-form navbar-right" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" />
+						</div> <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+					<ul class="nav navbar-nav navbar-left">
+						
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Style<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">抒情</a>
+								</li>
+								<li>
+									<a href="#">搖滾</a>
+								</li>
+								<li>
+									<a href="#">R&B</a>
+								</li>
+								<li>
+									<a href="#">洗腦</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">Update</a>
+						</li>
+					</ul>
+				</div>
+				
+			</nav>
+			<div class="row clearfix">
+		<div class="col-md-12 column">
+			<h3>
+				hello
+			</h3>
+			</div>
+		</div>
+
+			<div class="jumbotron" style="background: url('http://www.drew.edu/undergraduate/wp-content/uploads/sites/106/music1.jpg') no-repeat" >
+				<h1>
+					Music.db
+				</h1>
+				<p>
+					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large" href="#">Learn more</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<h3>
+				Member
+			</h3>
+		</div>
+	</div>
+
+	<div class="row clearfix">
+		<div class="col-md-4 column">
+			<img alt="140x140" src="http://goo.gl/d7Kx8S" class="img-rounded" style=" width: 240px;height: 240px" />
+			<h2>
+				王天郁
+			</h2>
+			<p>
+				b01705018
+			</p>
+			<p>
+				<a class="btn" href="#"></a>
+			</p>
+		</div>
+		<div class="col-md-4 column">
+			<img alt="140x140" src="http://goo.gl/RmexZX" class="img-rounded" style="width: 240px;height: 240px" />
+			<h2>
+				蕭志融
+			</h2>
+			<p>
+				b01705031
+			</p>
+			<p>
+				<a class="btn" href="#"></a>
+			</p>
+		</div>
+		<div class="col-md-4 column">
+			<img alt="140x140" src="http://goo.gl/ZU0n6F" class="img-rounded" style="width: 240px;height: 240px"/>
+			<h2>
+				楊依璇
+			</h2>
+			<p>
+				b01705029
+			</p>
+			<p>
+				<a class="btn" href="#"></a>
+			</p>
+		</div>
+
+	</div>
+</div>
+
+
+
+		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+</body>
+</html>

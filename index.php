b3a9adaa1d9a1c479edacdd28715671a04fa4716
mikeyandleo <b01705031@ntu.edu.tw>
2015-01-20 17:22:42 +0800

    add success.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
 					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
 				</p>
 				<p>
-					<a class="btn btn-primary btn-large" href="guide.php">Learn more</a>
+					<a class="btn btn-info btn-large" href="guide.php">Learn more</a>
 				</p>
 			</div>
 		</div>

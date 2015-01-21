9f4277b89e42491cff52a836ccf2d716169d76df
mikeyandleo <b01705031@ntu.edu.tw>
2015-01-21 11:17:02 +0800

    11:17

--- update.php
+++ update.php
@@ -19,9 +19,7 @@
                                 
                                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                                         <ul class="nav navbar-nav">
-                                            <li >
-                                                <a href="sec.php">Sec</a>
-                                            </li>
+                                            
                                             <li >
                                                 <a href="guide.php">Guide</a>
                                             </li>
